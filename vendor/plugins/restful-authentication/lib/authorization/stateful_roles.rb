2e2a4a2bc69e26e0dddf2bda3989aa259105c6fa
Jim Jeffers <jim@sumocreations.com>
2009-05-14 15:54:22 -0700

    Added exception_notification to project.

--- vendor/plugins/restful-authentication/lib/authorization/stateful_roles.rb
+++ vendor/plugins/restful-authentication/lib/authorization/stateful_roles.rb
@@ -9,30 +9,30 @@ module Authorization
       recipient.class_eval do
         include StatefulRolesInstanceMethods
         
-        acts_as_state_machine :initial => :pending
-        state :passive
-        state :pending, :enter => :make_activation_code
-        state :active,  :enter => :do_activate
-        state :suspended
-        state :deleted, :enter => :do_delete
+        aasm_initial_state :initial => :pending
+        aasm_state :passive
+        aasm_state :pending, :enter => :make_activation_code
+        aasm_state :active,  :enter => :do_activate
+        aasm_state :suspended
+        aasm_state :deleted, :enter => :do_delete
 
-        event :register do
+        aasm_event :register do
           transitions :from => :passive, :to => :pending, :guard => Proc.new {|u| !(u.crypted_password.blank? && u.password.blank?) }
         end
         
-        event :activate do
+        aasm_event :activate do
           transitions :from => :pending, :to => :active 
         end
         
-        event :suspend do
+        aasm_event :suspend do
           transitions :from => [:passive, :pending, :active], :to => :suspended
         end
         
-        event :delete do
+        aasm_event :delete do
           transitions :from => [:passive, :pending, :active, :suspended], :to => :deleted
         end
 
-        event :unsuspend do
+        aasm_event :unsuspend do
           transitions :from => :suspended, :to => :active,  :guard => Proc.new {|u| !u.activated_at.blank? }
           transitions :from => :suspended, :to => :pending, :guard => Proc.new {|u| !u.activation_code.blank? }
           transitions :from => :suspended, :to => :passive
