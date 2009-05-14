c331a8dcc746ed5e8d4ce92f93ff1089396db2bb
Jim Jeffers <jim@sumocreations.com>
2009-05-14 15:36:12 -0700

    Setup restful authentication w/ User and Session models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users, :member => { :suspend   => :put,
+                                     :unsuspend => :put,
+                                     :purge     => :delete }
+  map.resource :session
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
