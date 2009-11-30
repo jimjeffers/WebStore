3ff85e9ac037ffd55d0c8a5e5ec6d252592676bb
Jim Jeffers <jim@sumocreations.com>
2009-12-01 07:33:02 +1000

    Added production paypal credentials.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
 config.cache_classes = true
 
 # Full error reports are disabled and caching is turned on
@@ -9,34 +7,16 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-# See everything in the log (default is :info)
-# config.log_level = :debug
-
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
-
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
-
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
-
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-
-# Enable threaded mode
-# config.threadsafe!
-
 config.after_initialize do
-  ActiveMerchant::Billing::Base.mode = :test
+  ActiveMerchant::Billing::Base.mode = :production
 end
 
 config.to_prepare do
   OrderTransaction.gateway = 
     ActiveMerchant::Billing::PaypalGateway.new(
-      :login    => 'jim_1247017987_biz_api1.sumocreations.com',
-      :password => '1247017994',
-      :signature => 'ANvXwog7Kwq3lTkwtkscvaGLRuOVABeesC2ctzu38Pfsfe9GwUH83Ths'
+      :login    => 'cactussportsasu_api1.gmail.com',
+      :password => 'FGQJQSQX8YF82BEL',
+      :signature => 'ALP6VdoB-Xgp8Ii3v8C68Tc8xt3BAqSQTqPrvCLoKbou5PVkCE2FfFos'
     )
 end
 
